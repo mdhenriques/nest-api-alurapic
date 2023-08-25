fcebd61f0d5684729e221ce0f181825859c7f5e8
mdhenriques <md.henriques@outlook.com>
2023-08-25 14:19:30 -0300

    Adicionanando decorators de validacao na entidade de usuario

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,8 +1,22 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
 export class Usuario {
     id: number;
+
+    @IsNotEmpty()
+    @IsString()
     nomeDeUsuario: string;
+
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty()
     senha: string;
+
+    @IsNotEmpty({
+        message: 'Nome completo e obrigatorio'
+    })
     nomeCompleto: string;
+
     dataDeEntrada: Date;
 }
